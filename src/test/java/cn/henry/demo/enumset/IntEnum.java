2ef227aa9dc0325448f7304cd3ff782e69bd013b
hang <xiaohang.duan@quantgroup.cn>
2019-09-01 20:00:54 +0800

    reflect

--- src/test/java/cn/henry/demo/enumset/IntEnum.java
+++ src/test/java/cn/henry/demo/enumset/IntEnum.java
@@ -1,9 +1,9 @@
 package cn.henry.demo.enumset;
 
 public final class IntEnum {
-    public static final int STYLE_BOLD = 1 << 0;
-    public static final int STYLE_ITALIC = 1 << 1;
-    public static final int STYLE_UNDERLINE = 1 << 2;
+    public static final int STYLE_BOLD = 1 << 0; //1
+    public static final int STYLE_ITALIC = 1 << 1; //2
+    public static final int STYLE_UNDERLINE = 1 << 2; //4
     public static final int STYLE_STRIKETHROUGH = 1 << 3;  // 8
 
     public static void applyStyles(int styles) {
---
+++ src/test/java/cn/henry/demo/reflect/TestReflect.java
@@ -0,0 +1,24 @@
+package cn.henry.demo.reflect;
+
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+public class TestReflect {
+
+    @Test
+    public void test() throws Exception {
+        /**
+         * Class类:反射的核心类，可以获取类的属性，方法等内容信息。 (java.lang #Class)
+         * Field类:Java.lang.reflect.表示类的属性，可以获取和设置类的中属性值。( (java.lang.reflect))
+         * Method类:Java.lang.reflect。表示类的方法，它可以用来获取类中方法的信息或者执行方法
+         * Construcor类:Java.lang.reflect。表示类的构造方法。 (java.lang.reflect)
+         */
+        Class<?> aClass = Class.forName("cn.henry.demo.enumset.IntEnum");
+        Field[] fields = aClass.getDeclaredFields();
+        for (Field f : fields){
+            String name = f.getName();
+            System.out.println(f.get(name));
+        }
+    }
+}
