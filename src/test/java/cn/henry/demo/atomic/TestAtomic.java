d58e75ac0dac01951114ad9f5a0e8bdbbf4cd6ed
hang <xiaohang.duan@quantgroup.cn>
2019-10-29 19:24:41 +0800

    atomicIntegerArray method

--- src/test/java/cn/henry/demo/atomic/TestAtomic.java
+++ src/test/java/cn/henry/demo/atomic/TestAtomic.java
@@ -5,12 +5,15 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 import java.util.stream.Collectors;
 
 public class TestAtomic {
 
     @Test
     public void test() {
+
+        System.out.println("---------------------AtomicInteger start-----------------------");
         AtomicInteger integer = new AtomicInteger(2);
 
         int getAndIncrement = integer.getAndIncrement();
@@ -26,6 +29,21 @@ public class TestAtomic {
 
         boolean b = integer.compareAndSet(5, 1);
         System.out.println("b : " + b + "; integer : " + integer);
+        System.out.println("---------------------AtomicInteger end-----------------------");
+
+
+        System.out.println("---------------------AtomicIntegerArray start-----------------------");
+        int[] arr = {9, 5, 7, 2};
+        AtomicIntegerArray integerArray = new AtomicIntegerArray(arr);
+
+        //获取 index=i 位置元素的值，并让该位置的元素自增
+        int andIncrement = integerArray.getAndIncrement(2);
+        System.out.println("AtomicIntegerArray_getAndIncrement : " + andIncrement + "; integerArray : " + integerArray);
+        int andGet = integerArray.incrementAndGet(2);
+        System.out.println("AtomicIntegerArray_incrementAndGet : " + andGet + "; incrementAndGet : " + integerArray);
+        int arrayAndAdd = integerArray.getAndAdd(2, 5);
+        System.out.println("AtomicIntegerArray_getAndAdd : " + arrayAndAdd + "; incrementAndGet : " + integerArray);
+        System.out.println("---------------------AtomicIntegerArray end-----------------------");
 
 
         List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 8, 10);
