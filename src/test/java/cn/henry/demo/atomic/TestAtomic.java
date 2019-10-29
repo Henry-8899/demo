d54febb8fde6f34ac66c4fcd95a992691f419abf
hang <xiaohang.duan@quantgroup.cn>
2019-10-29 15:41:13 +0800

    atomicInteger method

--- src/test/java/cn/henry/demo/atomic/TestAtomic.java
+++ src/test/java/cn/henry/demo/atomic/TestAtomic.java
@@ -12,9 +12,21 @@ public class TestAtomic {
     @Test
     public void test() {
         AtomicInteger integer = new AtomicInteger(2);
-        int i = integer.getAndIncrement();
 
-        System.out.println(i);
+        int getAndIncrement = integer.getAndIncrement();
+        System.out.println("getAndIncrement : " + getAndIncrement + "; integer : " + integer);
+        int incrementAndGet = integer.incrementAndGet();
+        System.out.println("incrementAndGet : " + incrementAndGet + "; integer : " + integer);
+
+        int andSet = integer.getAndSet(10);
+        System.out.println("andSet : " + andSet + "; integer : " + integer);
+
+        int andAdd = integer.getAndAdd(5);
+        System.out.println("andAdd : " + andAdd + "; integer : " + integer);
+
+        boolean b = integer.compareAndSet(5, 1);
+        System.out.println("b : " + b + "; integer : " + integer);
+
 
         List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 8, 10);
         List<Integer> collect = integers.stream().filter(e -> e > 4).collect(Collectors.toList());
