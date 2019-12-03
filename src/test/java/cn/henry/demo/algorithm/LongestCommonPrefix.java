89ecbc85b2229fc879c338b07732fee0a4280272
hang <xiaohang.duan@quantgroup.cn>
2019-12-03 15:28:04 +0800

    longest common prefix

---
+++ src/test/java/cn/henry/demo/algorithm/LongestCommonPrefix.java
@@ -0,0 +1,34 @@
+package cn.henry.demo.algorithm;
+
+import org.junit.Test;
+
+public class LongestCommonPrefix {
+
+
+    /**
+     * 编写一个函数来查找字符串数组中的最长公共前缀。
+     *
+     * 如果不存在公共前缀，返回空字符串 ““。
+     *
+     * 示例 1:
+     * 输入: ["flower","flow","flight"]
+     * 输出: "fl"
+     */
+    @Test
+    public void test() {
+        String[] strings = {"ab","cd"};
+        String s = longestCommonPrefix(strings);
+        System.out.println(s);
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
+}
