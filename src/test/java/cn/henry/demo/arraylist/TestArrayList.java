fb58bd8e2f2ac2b8fb3c4311201f155188bf668a
hang <xiaohang.duan@quantgroup.cn>
2019-12-24 17:26:31 +0800

    sublist index range

--- src/test/java/cn/henry/demo/arraylist/TestArrayList.java
+++ src/test/java/cn/henry/demo/arraylist/TestArrayList.java
@@ -3,6 +3,8 @@ package cn.henry.demo.arraylist;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TestArrayList {
 
@@ -16,5 +18,17 @@ public class TestArrayList {
         objects.add(1);
         objects.add(2);
         objects.add(20);
+
+
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        //前闭后开 [0,2) 这样一个区间
+        List<Integer> integers1 = integers.subList(0, 2);
+        List<Integer> integers2 = integers.subList(2, 4);
+        System.out.println("end");
+
+
+
+
     }
 }
