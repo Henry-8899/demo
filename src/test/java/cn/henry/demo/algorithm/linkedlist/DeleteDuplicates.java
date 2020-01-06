18c1a27cadd8bf1f822d01d456d90916c102ff1a
hang <xiaohang.duan@quantgroup.cn>
2020-01-06 11:53:43 +0800

    delete duplicates

---
+++ src/test/java/cn/henry/demo/algorithm/linkedlist/DeleteDuplicates.java
@@ -0,0 +1,48 @@
+package cn.henry.demo.algorithm.linkedlist;
+
+/**
+ * 删除排序链表中的重复元素 leetcode 83
+ *
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->1->2
+ * 输出: 1->2
+ * 示例 2:
+ *
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ */
+public class DeleteDuplicates {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 复杂度分析
+     *
+     * 时间复杂度：O(n)，因为列表中的每个结点都检查一次以确定它是否重复，所以总运行时间为 O(n)，其中 n 是列表中的结点数。
+     *
+     * 空间复杂度：O(1)，没有使用额外的空间。
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
