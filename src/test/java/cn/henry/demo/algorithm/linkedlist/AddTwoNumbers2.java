502fc907546fa3585f5eac7f6b22a1f2c1eff199
hang <xiaohang.duan@quantgroup.cn>
2020-01-07 13:50:45 +0800

    add two numbers 2

---
+++ src/test/java/cn/henry/demo/algorithm/linkedlist/AddTwoNumbers2.java
@@ -0,0 +1,65 @@
+package cn.henry.demo.algorithm.linkedlist;
+
+import java.util.Stack;
+
+/**
+ * 两数相加 II leetcode 445
+ */
+public class AddTwoNumbers2 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 给定两个非空链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储单个数字。将这两数相加会返回一个新的链表。
+     *
+     * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+     *
+     * 进阶:
+     * 如果输入链表不能修改该如何处理？换句话说，你不能对列表中的节点进行翻转。
+     *
+     * 示例:
+     * 输入: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * 输出: 7 -> 8 -> 0 -> 7
+     *
+     * 耗时 8ms
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> l1Stack = buildStack(l1);
+        Stack<Integer> l2Stack = buildStack(l2);
+        ListNode head = new ListNode(-1);
+        int carry = 0;
+        while (!l1Stack.isEmpty() || !l2Stack.isEmpty() || carry != 0) {
+            int x = l1Stack.isEmpty() ? 0 : l1Stack.pop();
+            int y = l2Stack.isEmpty() ? 0 : l2Stack.pop();
+            int sum = x + y + carry;
+            ListNode node = new ListNode(sum % 10);
+
+            //核心：交换位置 采用头插
+            node.next = head.next;
+            head.next = node;
+
+            carry = sum / 10;
+        }
+        return head.next;
+    }
+
+    //利用压栈，先进先出原理
+    private Stack<Integer> buildStack(ListNode l) {
+        Stack<Integer> stack = new Stack<>();
+        while (l != null) {
+            stack.push(l.val);
+            l = l.next;
+        }
+        return stack;
+    }
+}
